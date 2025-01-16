a2cd597b6d1c42e6e7045553f76a3e6463e301df
James <justjamescreative@outlook.com>
2025-01-15 20:21:42 -0500

    Updated UserLoginApplication
    Changed the name of the application to adhere to the project specs.

---
+++ src/UserLoginApplication.java
@@ -0,0 +1,9 @@
+public class UserLoginApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter your email:");
+
+        System.out.println("Enter your password:");
+    }
+}
