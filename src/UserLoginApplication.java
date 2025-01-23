f37ce65e8945ac5ada2484669781ffb512434a3b
James <justjamescreative@outlook.com>
2025-01-22 23:25:26 -0500

    Updated UserLoginApplication

--- src/UserLoginApplication.java
+++ src/UserLoginApplication.java
@@ -1,7 +1,25 @@
 public class UserLoginApplication {
 
     public static void main(String[] args) {
+        UserService userService = new UserService();
+        userService.getUsersFromFile("data.txt");
 
-        // User login interface for the console
+        int userAttempts = 5;
+
+        while(userAttempts > 0) {
+            String username = userService.askForUsername();
+            String password = userService.askForPassword();
+
+            if (userService.validateUsername(username) && userService.validatePassword(password)) {
+                String usersName = userService.getUsersName(username);
+                System.out.println("Welcome: " + usersName);
+                return;
+            } else {
+                System.out.println("Invalid login, please try again.");
+                userAttempts--;
+            }
+        }
+
+        System.out.println("Too many failed login attempts, you are now locked out.");
     }
 }
--- src/UserService.java
+++ src/UserService.java
@@ -1,7 +1,76 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class UserService {
+    public User[] users = new User[4];
 
-    // Array of User objects
-    // File reader
-    // User info validation
+    public void getUsersFromFile(String filePath) {
+        int index = 0;
 
+        try {
+            BufferedReader userFileReader = new BufferedReader(new FileReader(filePath));
+            String line;
+            while((line = userFileReader.readLine()) != null && index < users.length) {
+                String[] fields = line.split(",");
+                if (fields.length == 3) {
+                    String username = fields[0];
+                    String password = fields[1];
+                    String name = fields[2];
+
+                    users[index] = new User(username, password, name);
+                    index++;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Oops, file not found.");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Oops, file could not be read.");
+            e.printStackTrace();
+        }
+
+    }
+
+    public boolean validateUsername(String username) {
+        for (User user : users) {
+            if (user.getUsername().equalsIgnoreCase(username)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean validatePassword(String password) {
+        for (User user : users) {
+            if (user.getPassword().equals(password)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String askForUsername() {
+        System.out.println("Enter your email: ");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    public String askForPassword() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your password: ");
+        return scanner.nextLine();
+    }
+
+    public String getUsersName(String username) {
+        for (User user : users) {
+            if (user.getUsername().equalsIgnoreCase(username)) {
+                return user.getName();
+            }
+        }
+        return null;
+    }
 }
+
